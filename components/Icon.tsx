f22fbb2871ef3386255d96caf8338e5919212d89
可达鸭 <3443833799@qq.com>
2026-03-04 04:17:14 +0800

    fix: correct Icon component syntax for Next.js build
    - Changed arrow functions to function declarations
    - Fixes TypeScript/JSX parsing error in Vercel build
    - All icon exports now use proper function syntax

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as Icons from 'lucide-react'
-import { LucideProps } from 'lucide-react'
+import type { LucideProps } from 'lucide-react'
 
 interface IconProps extends LucideProps {
   name: keyof typeof Icons
@@ -19,24 +19,86 @@ export default function Icon({ name, ...props }: IconProps) {
 }
 
 // 导出常用的 icons 作为 Client Components
-export const BarChart3 = (props: LucideProps) => <Icon name="BarChart3" {...props} />
-export const FileText = (props: LucideProps) => <Icon name="FileText" {...props} />
-export const Twitter = (props: LucideProps) => <Icon name="Twitter" {...props} />
-export const BookOpen = (props: LucideProps) => <Icon name="BookOpen" {...props} />
-export const Terminal = (props: LucideProps) => <Icon name="Terminal" {...props} />
-export const Radio = (props: LucideProps) => <Icon name="Radio" {...props} />
-export const TrendingUp = (props: LucideProps) => <Icon name="TrendingUp" {...props} />
-export const ArrowLeft = (props: LucideProps) => <Icon name="ArrowLeft" {...props} />
-export const ExternalLink = (props: LucideProps) => <Icon name="ExternalLink" {...props} />
-export const Clock = (props: LucideProps) => <Icon name="Clock" {...props} />
-export const User = (props: LucideProps) => <Icon name="User" {...props} />
-export const Hash = (props: LucideProps) => <Icon name="Hash" {...props} />
-export const Calendar = (props: LucideProps) => <Icon name="Calendar" {...props} />
-export const Sparkles = (props: LucideProps) => <Icon name="Sparkles" {...props} />
-export const Code = (props: LucideProps) => <Icon name="Code" {...props} />
-export const AlertCircle = (props: LucideProps) => <Icon name="AlertCircle" {...props} />
-export const Zap = (props: LucideProps) => <Icon name="Zap" {...props} />
-export const Eye = (props: LucideProps) => <Icon name="Eye" {...props} />
-export const Trash2 = (props: LucideProps) => <Icon name="Trash2" {...props} />
-export const Loader2 = (props: LucideProps) => <Icon name="Loader2" {...props} />
-export const ArrowUpDown = (props: LucideProps) => <Icon name="ArrowUpDown" {...props} />
+export function BarChart3(props: LucideProps) {
+  return <Icon name="BarChart3" {...props} />
+}
+
+export function FileText(props: LucideProps) {
+  return <Icon name="FileText" {...props} />
+}
+
+export function Twitter(props: LucideProps) {
+  return <Icon name="Twitter" {...props} />
+}
+
+export function BookOpen(props: LucideProps) {
+  return <Icon name="BookOpen" {...props} />
+}
+
+export function Terminal(props: LucideProps) {
+  return <Icon name="Terminal" {...props} />
+}
+
+export function Radio(props: LucideProps) {
+  return <Icon name="Radio" {...props} />
+}
+
+export function TrendingUp(props: LucideProps) {
+  return <Icon name="TrendingUp" {...props} />
+}
+
+export function ArrowLeft(props: LucideProps) {
+  return <Icon name="ArrowLeft" {...props} />
+}
+
+export function ExternalLink(props: LucideProps) {
+  return <Icon name="ExternalLink" {...props} />
+}
+
+export function Clock(props: LucideProps) {
+  return <Icon name="Clock" {...props} />
+}
+
+export function User(props: LucideProps) {
+  return <Icon name="User" {...props} />
+}
+
+export function Hash(props: LucideProps) {
+  return <Icon name="Hash" {...props} />
+}
+
+export function Calendar(props: LucideProps) {
+  return <Icon name="Calendar" {...props} />
+}
+
+export function Sparkles(props: LucideProps) {
+  return <Icon name="Sparkles" {...props} />
+}
+
+export function Code(props: LucideProps) {
+  return <Icon name="Code" {...props} />
+}
+
+export function AlertCircle(props: LucideProps) {
+  return <Icon name="AlertCircle" {...props} />
+}
+
+export function Zap(props: LucideProps) {
+  return <Icon name="Zap" {...props} />
+}
+
+export function Eye(props: LucideProps) {
+  return <Icon name="Eye" {...props} />
+}
+
+export function Trash2(props: LucideProps) {
+  return <Icon name="Trash2" {...props} />
+}
+
+export function Loader2(props: LucideProps) {
+  return <Icon name="Loader2" {...props} />
+}
+
+export function ArrowUpDown(props: LucideProps) {
+  return <Icon name="ArrowUpDown" {...props} />
+}
